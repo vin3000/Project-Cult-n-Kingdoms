dadbd49bc1914ccd879d1f383919825dbaa8ed42
Nostiny <153897581+Nostiny@users.noreply.github.com>
2024-01-10 14:46:53 +0100

    Jumping

---
+++ Assets/playerJump.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerJump : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float jumpSpeed, xOffset, yOffset, xSize, ySize;
+    public bool isGrounded;
+    public LayerMask groundMask;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapBox(new Vector2(transform.position.x + xOffset, transform.position.y + yOffset), new Vector2(xSize, ySize), 0f, groundMask);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(new Vector2(transform.position.x + xOffset, transform.position.y + yOffset), new Vector2(xSize, ySize));
+    }
+
+}
---
+++ Assets/playerMovment.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class playerWalk : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveInputH;
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        moveInputH = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInputH * speed, rb.velocity.y);
+
+        if(moveInputH != 0)
+        {
+
+            playerVariebels.isWalking = true;
+
+        }
+        else
+        {
+            playerVariebels.isWalking = false;
+        }
+
+
+              
+
+    }
+
+     
+
+}
