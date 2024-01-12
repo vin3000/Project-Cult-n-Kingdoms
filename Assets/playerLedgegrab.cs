c04075aa0e1803b5d7d1a7f0bd4f15bd0196d381
Nostiny <153897581+Nostiny@users.noreply.github.com>
2024-01-12 16:35:05 +0100

    ledgegrab(not working)

--- Assets/playerLedgegrab.cs
+++ Assets/playerLedgegrab.cs
@@ -21,14 +21,33 @@ public class playerLedgegrab : MonoBehaviour
     void Update()
     {
         greenBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (greenXOffset * transform.localScale.x), transform.position.y + greenYOffset), new Vector2(greenXSize, greenYSize), 0f, groundMask);
-        RedBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y + redYOffset), new Vector2(redXSize, redYSize), 0f, groundMask);
+        RedBox = Physics2D.OverlapBox(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y + redYOffset), new Vector2(redXSize, redYSize), 0f, groundMask); 
+
+        if(greenBox && !RedBox && !playerVariebels.isGrabbing && playerVariebels.isJumping)
+        {
+            playerVariebels.isGrabbing = true;
+        }
+        if (playerVariebels.isGrabbing)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+            rb.gravityScale = 0f;
+        }
+
+    }
+
+    public void Changepos()
+    {
+        transform.position = new Vector2(transform.position.x + (0.5f * transform.localScale.x), transform.position.y + 0.4f);
+        rb.gravityScale = startingGrav;
+        playerVariebels.isGrabbing = false;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(new Vector2(transform.position.x + (redXOffset * transform.localScale.x), transform.position.y + redYOffset), new Vector2(redXSize, redYSize));
-
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(new Vector2(transform.position.x + (greenXOffset * transform.localScale.x), transform.position.y + greenYOffset), new Vector2(greenXSize, greenYSize));
     }
 
 }
