369b2dc09ba6eb40ca8aeef7229f9285ded8035b
GrandpaMiles <milton.hernvald@elev.ga.lbs.se>
2024-01-12 14:31:30 +0100

    everything

--- Assets/interact range.cs
+++ Assets/interact range.cs
@@ -1,20 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Mainscene : MonoBehaviour
-{
-    public void PlayGame() 
-    {
-        SceneManager.LoadSceneAsync(1);
-        new WaitForSeconds(5);
-}
+public class interactrange : MonoBehaviour, IInteractable {
+    public void Interact() {
+        Debug.Log(Random.Range(0, 100));
+    }
     // Start is called before the first frame update
     void Start()
     {
         
-    } 
+    }
 
     // Update is called once per frame
     void Update()
