aae48069d37cb2f27a0e32bc165ccb38a3a59161
vin3000 <vincent.romer@elev.ga.lbs.se>
2024-01-18 16:36:00 +0100

    Revert "La till Silver's parallax"
    This reverts commit 83291d7e0d91adaebad3492af02ca1cec07c6325.

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
@@ -13,17 +12,14 @@ public class Parallax : MonoBehaviour
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-    void Update()
+    void FixedUpdate()
     {
-        if (cam.transform.position.x < 61)
-        {
-            float temp = (cam.transform.position.x * (1 - parallaxEffect));
-            float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
 
-            transform.position = new Vector3(startpos + dist, 4.5f, transform.position.z);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-            if (temp > startpos + length) startpos += length;
-            else if (temp < startpos - length) startpos -= length;
-        }
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
