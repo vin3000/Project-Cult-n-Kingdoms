20b6b217671a2578ebda7c89cdae49a2307a3c74
FumeGrandpa <victor.jeppsson@elev.ga.lbs.se>
2024-01-31 13:31:35 +0100

    Test text popup

--- Assets/Scripts/Interactionsystem/InteractPopup.cs
+++ Assets/Scripts/Interactionsystem/InteractPopup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,12 +11,11 @@ public class InteractPopup : MonoBehaviour
     public UnityEvent interaAction;
     public GameObject textPopUp;
     float alpha = 0;
-    public SpriteRenderer spriteRenderer;
+    public TextMeshPro TMP;
 
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();   
     }
 
     // Update is called once per frame
@@ -24,12 +24,12 @@ public class InteractPopup : MonoBehaviour
         if (isInRange && alpha < 1) //if player is in range
         {
             alpha += 2 * Time.deltaTime;
-            spriteRenderer.color = new Color(1, 1, 1, alpha);
+            TMP.color = new Color(1, 1, 1, alpha);
         }
         if (!isInRange && alpha > 0)
         {
             alpha -= 2 * Time.deltaTime;
-            spriteRenderer.color = new Color(1, 1, 1, alpha);
+            TMP.color = new Color(1, 1, 1, alpha);
         }
     }
 
